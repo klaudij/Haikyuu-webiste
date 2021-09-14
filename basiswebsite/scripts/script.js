6d28544d8763beaad353333fe3e922e0891d9b27
Klaudija✧˖*°࿐ <89772847+klaudij@users.noreply.github.com>
2021-09-14 14:01:26 +0200

    Add files via upload

---
+++ basiswebsite/scripts/script.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+
+
+// JSS code van ---->https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
+//Get the button
+var mybutton = document.getElementById("back2Top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
